9f984db4c20f876e0454670b5b7525badd39dc24
Tosasitill <66861785+tosasitill@users.noreply.github.com>
2023-02-10 23:53:02 +0800

    Delete true.py

--- true.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = 'https://sourceforge.net/projects/<PROJECT_NAME>/files/latest/download'
-response = requests.head(url, allow_redirects=True)
-download_url = response.headers['location']
